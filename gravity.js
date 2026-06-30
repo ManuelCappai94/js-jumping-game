028606854d29cef0830d56acab3de1d6554ebf1b
GNevesRPereira <92853292+GNevesRPereira@users.noreply.github.com>
2026-06-30 19:41:02 +0100

    Create gravity.js

---
+++ gravity.js
@@ -0,0 +1,52 @@
+//All this need to be revised
+var myGamePiece;
+
+function startGame() {
+    myGamePiece = new component(30, 30, "red", 80, 75);
+    myGameArea.start();
+}
+
+var myGameArea = {
+    canvas : document.createElement("canvas"),
+    start : function() {
+        this.canvas.width = 480;
+        this.canvas.height = 270;
+        this.context = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        this.interval = setInterval(updateGameArea, 20);        
+    },
+    stop : function() {
+        clearInterval(this.interval);
+    },    
+    clear : function() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+}
+
+//This needs to be possibly implemented in the player
+function playerGravity(width, height, color, x, y, type) {
+    this.type = type;
+    this.width = width;
+    this.x = x;
+    this.y = y;
+    this.speedX = 0;
+    this.speedY = 0;
+    this.gravity = 0.05;
+    this.gravitySpeed = 0;
+    this.update = function () {
+        ctx = myGameArea.context;
+        ctx.fillStyle = color;
+        ctx.fillrect(thisx, this.width, this.height);
+    }
+    this.newPos = function () {
+        this.gravitySpeed += this.gravity;
+        this.x += this.speedX;
+        this.y += this.speedY + this.gravitySpeed;
+    }
+}
+
+function updateGameArea(){
+    myGameArea.clear();
+    myGameArea.newPos();
+    myGameArea.update();
+}
