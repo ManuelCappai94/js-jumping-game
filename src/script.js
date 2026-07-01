071b67d6e85057678f45d4c31b2a1a82503b2295
GNevesRPereira <92853292+GNevesRPereira@users.noreply.github.com>
2026-07-01 16:46:14 +0100

    Score system

--- src/script.js
+++ src/script.js
@@ -1,7 +1,7 @@
 import { menuMusic, mainMusic, victoryMusic, playMenuSound, defeatMusic, playMusic, stopMusic } from "./audio.js";
 import { updatePlayer, player, keys } from "./player.js";
 import { spawnObstacles, updateObstacles } from "./obstacles.js";
-import { boundaryCollision } from "./collision.js";
+import { boundaryCollision, collision  } from "./collision.js";
 
 const gameCanvas = document.querySelector(".game");
 const mainMenu = gameCanvas.querySelector(".main-menu");
@@ -67,10 +67,11 @@ function renderPlayer() {
     }
 }
 
-function displayScore() {
+let score = 0;
 
-    let score = 0;
-
+function addScore(points) {
+    score += points;
+    console.log(score);
 }
 
 let lastTime = 0;
